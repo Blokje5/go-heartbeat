59a9df4d0cab86a78799af7930b9d166976f077a
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2022-07-31 17:05:25 +0200

    chore: Rename Methods and Structs to avoid to much duplicate heartbeat references (#2)

--- examples/deadlock-with-heartbeat/main.go
+++ examples/deadlock-with-heartbeat/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	cfg := heartbeat.NewHeartBeatConfig(100 * time.Millisecond, 200 * time.Millisecond)
-	beat := heartbeat.NewHeartbeat(cfg)
-	monitor := heartbeat.NewHeartBeatMonitor(beat, cfg)
+	cfg := heartbeat.NewConfig(100*time.Millisecond, 200*time.Millisecond)
+	beat := heartbeat.New(cfg)
+	monitor := heartbeat.NewMonitor(beat, cfg)
 
 	intStream := make(chan int)
 	unreadStream := make(chan interface{})
@@ -24,22 +24,22 @@ func main() {
 		defer close(unreadStream)
 		for i := range intStream {
 			select {
-				case <-beat.PulseInterval():
-					// notify on heartbeat interval
-					if err := beat.SendPulse(); err != nil {
-						return
-					}
-				default:
-					// do work
-					fmt.Println(i)
-					if i > 9 {
-						unreadStream <- struct{}{}
-					}
+			case <-beat.PulseInterval():
+				// notify on heartbeat interval
+				if err := beat.SendPulse(); err != nil {
+					return
+				}
+			default:
+				// do work
+				fmt.Println(i)
+				if i > 9 {
+					unreadStream <- struct{}{}
+				}
 			}
 
 		}
 	}()
-	
+
 	for i := 0; i < 100; i++ {
 		select {
 		case intStream <- i:
@@ -54,5 +54,3 @@ func main() {
 
 	close(intStream)
 }
-
-
